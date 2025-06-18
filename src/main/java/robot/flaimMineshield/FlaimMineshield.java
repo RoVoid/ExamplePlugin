08c2914cdf63c0ddb81d9c579b678147839b2787
RoVoid <officialrobot24@gmail.com>
2025-06-18 18:08:02 +0400

    recipe title fix

--- src/main/java/robot/flaimMineshield/FlaimMineshield.java
+++ src/main/java/robot/flaimMineshield/FlaimMineshield.java
@@ -1,13 +1,18 @@
 package robot.flaimMineshield;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.Style;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FlaimMineshield extends JavaPlugin implements Listener {
@@ -23,22 +28,20 @@ public final class FlaimMineshield extends JavaPlugin implements Listener {
         BarrierManager = new BarrierManager();
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
-
-    @EventHandler
-    public void PlayerJoin(PlayerJoinEvent event) {
-    }
-
     @EventHandler
     public void onPrepare(PrepareItemCraftEvent event) {
         if (!(event.getView().getPlayer() instanceof Player)) return;
 
-        var recipe = event.getRecipe();
+        Recipe recipe = event.getRecipe();
         if (recipe != null && recipe.getResult().getType() == Material.IRON_AXE) {
-            event.getInventory().setResult(new ItemStack(Material.BARRIER));
+            ItemStack itemStack = new ItemStack(Material.BARRIER);
+            ItemMeta meta = itemStack.getItemMeta();
+            if (meta != null) {
+                meta.displayName(Component.text("Запрещено", Style.style(NamedTextColor.RED).decoration(TextDecoration.ITALIC, false)));
+                itemStack.setItemMeta(meta);
+            }
+            event.getInventory().setResult(itemStack);
+
         }
     }
 
